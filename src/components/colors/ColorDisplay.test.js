299b4270c8ea8de4067e0e4b37a3af104428f6d7
sanvean74 <dlambertpdx@gmail.com>
2019-10-30 17:00:43 -0700

    all tests passing

--- src/components/colors/ColorDisplay.test.js
+++ src/components/colors/ColorDisplay.test.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import Enzyme from 'enzyme';
 import { shallow } from 'enzyme';
 import ColorDisplay from './ColorDisplay';
-import Adapter from 'enzyme-adapter-react-16';
 
-Enzyme.configure({ adapter: new Adapter() });
 describe('Color Display Component', () => {
   it('should render color', () => {
     const wrapper = shallow(<ColorDisplay backgroundColor='red'/>);
